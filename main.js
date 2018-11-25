130ce3b12713bf1cd3dfde5aba453dd0c5afdf5e
itsumoonazicode <dyingkouki@gmail.com>
2018-11-25 23:33:12 +0900

    localstorageからデータ表示処理追加

--- main.js
+++ main.js
@@ -1,6 +1,7 @@
 const myStorage = localStorage;
 
 const copyTexts = document.querySelectorAll(".copyText");
+const lists = document.getElementById("list-group");
 const copyBtn = document.getElementById("copyBtn");
 
 copyBtn.addEventListener("click", () => {
@@ -12,4 +13,20 @@ copyBtn.addEventListener("click", () => {
         i++;
     });
     myStorage.setItem("copytxt", JSON.stringify(data));
+    copyBtn.insertAdjacentHTML("beforebegin", "<p><input type='text' class='copyText form-control'></p>");
 });
+
+const parseCopyTxts = JSON.parse(myStorage.getItem("copytxt"));
+if(parseCopyTxts instanceof Object) {
+    let mappingValue = Object.keys(parseCopyTxts).map((key) => {
+        return parseCopyTxts[key];
+    });
+    // 配列からfalsyな値を排除する
+    mappingValue = mappingValue.filter(Boolean);
+    console.log(mappingValue);
+
+    for(let j = 0; j < mappingValue.length; j++) {
+        lists.insertAdjacentHTML("beforeend", "<li class='list-group-item'>" + mappingValue[j] + "</li>");
+        console.log(mappingValue[j]);
+    }
+}
